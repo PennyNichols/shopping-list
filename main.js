365f0def30c49752a9a30924071459194ca89d10
pnichols789 <pnichols789@gmail.com>
2022-09-07 08:15:26 -0500

    prepped to begin javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const alert = document.getElementById("alert");
+const newItem = document.getElementById("new-list-item");
+const submitBtn = document.getElementById("submit-btn");
+const container = document.querySelector(".list-container");
+const list = document.getElementById("shopping-list");
+const clearBtn = document.getElementById("clear-btn");
+
