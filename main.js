755bce203d06cbb520a9ae8fbf6597aa92c1bed1
pnichols789 <pnichols789@gmail.com>
2022-09-10 18:27:01 -0500

    -

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
 
-const form = document.querySelector('.add-item');
-const alert = document.querySelector('.alert');
-const newItem = document.getElementById('new-list-item');
-const submitBtn = document.querySelector('.submit-btn');
-const container = document.querySelector('.list-container');
-const list = document.querySelector('.list');
-const clearBtn = document.querySelector('.clear-btn');
+const form = document.querySelector(".form");
+const alert = document.querySelector(".alert");
+const listInput = document.getElementById("list-input");
+const priceInput = document.getElementById("price-input");
+const qtyInput = document.getElementById("qty-input");
+
+
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".list-container");
+const list = document.querySelector(".list");
+const clearBtn = document.querySelector(".clear-btn");
 
 let editElement;
+let editPrice;
+let editQty;
+let editTotal;
 let editFlag = false;
-let editID = '';
+let editID = "";
 
-// add item
+const strike = (e) => {
+
+}
+
 const addItem = (e) => {
-  e.preventDefault();
-  const title = newItem.value;
-  const id = new Date().getTime().toString();
+    e.preventDefault();
+	const value = listInput.value;
+    const price = priceInput.value;
+    const qty = qtyInput.value;
+    const total = (((price * 100) * qty) / 100).toFixed(2);
+	const id = new Date().getTime().toString();
 
-  if (title !== '' && !editFlag) {
-    const element = document.createElement('div');
-    let pKey = document.createAttribute('data-id');
-    pKey.value = id;
-    element.setAttributeNode(pKey);
-    element.classList.add('item');
-    element.innerHTML = `<p class="title">${title}</p>
+	if (value !== "" && !editFlag) {
+		const element = document.createElement("article");
+		let attr = document.createAttribute("data-id");
+		attr.value = id;
+		element.setAttributeNode(attr);
+		element.classList.add("list-item");
+		element.innerHTML = `<p class="title item">${value}</p>
+            <p class="price">${price}</p>
+            <p class="qty">${qty}</p>
+            <p class="total">${total}</p>
             <div class="btn-container">
               <button type="button" class="edit-btn">
                 <i class="fas fa-edit"></i>
@@ -33,183 +49,173 @@ const addItem = (e) => {
               </button>
             </div>
           `;
-    // add event listeners to both buttons;
-    const deleteBtn = element.querySelector('.delete-btn');
-    deleteBtn.addEventListener('click', deleteItem);
-    const editBtn = element.querySelector('.edit-btn');
-    editBtn.addEventListener('click', editItem);
+		const deleteBtn = element.querySelector(".delete-btn");
+		deleteBtn.addEventListener("click", deleteItem);
+		const editBtn = element.querySelector(".edit-btn");
+		editBtn.addEventListener("click", editItem);
 
-    // append child
-    list.appendChild(element);
-    // display alert
-    displayAlert('item added to the list', 'success');
-    // show container
-    container.classList.add('show-container');
-    // set local storage
-    addToLocalStorage(id, title);
-    // set back to default
-    setBackToDefault();
-  } else if (title !== '' && editFlag) {
-    editElement.innerHTML = title;
-    displayAlert('item changed', 'success');
+		list.appendChild(element);
+		displayAlert("item added", "success");
+		container.classList.add("show-container");
+		addToLocalStorage(id, value, price, qty, total);
+		setBackToDefault();
+	} else if (value !== "" && editFlag) {
+		editElement.innerHTML = value;
+        editPrice.innerHTML = price;
+        editQty.innerHTML = qty;
+        editTotal.innerHTML = total;
+		displayAlert("item altered", "success");
 
-    // edit  local storage
-    editLocalStorage(editID, title);
-    setBackToDefault();
-  } else {
-    displayAlert('please enter an item', 'danger');
-  }
+		editLocalStorage(editID, value, price, qty, total);
+		setBackToDefault();
+	} else {
+		displayAlert("please input item", "danger");
+	}
 };
-// display alert
 const displayAlert = (text, action) => {
-  alert.textContent = text;
-  alert.classList.add(`alert-${action}`);
-  // remove alert
-  setTimeout(function () {
-    alert.textContent = '';
-    alert.classList.remove(`alert-${action}`);
-  }, 1000);
+	alert.textContent = text;
+	alert.classList.add(`alert-${action}`);
+	setTimeout(function () {
+		alert.textContent = "";
+		alert.classList.remove(`alert-${action}`);
+	}, 1000);
 };
 
-// clear items
 const clearItems = () => {
-  const items = document.querySelectorAll('.item');
-  if (items.length > 0) {
-    items.forEach(function (item) {
-      list.removeChild(item);
-    });
-  }
-  container.classList.remove('show-container');
-  displayAlert('empty list', 'danger');
-  setBackToDefault();
-  localStorage.removeItem('list');
+	const items = document.querySelectorAll(".list-item");
+	if (items.length > 0) {
+		items.forEach(function (item) {
+			list.removeChild(item);
+		});
+	}
+	container.classList.remove("show-container");
+	displayAlert("list cleared", "danger");
+	setBackToDefault();
+	localStorage.removeItem("list");
 };
 
-// delete item
 
 const deleteItem = (e) => {
-  const element = e.currentTarget.parentElement.parentElement;
-  const id = element.dataset.id;
+	const element = e.currentTarget.parentElement.parentElement;
+	const id = element.dataset.id;
 
-  list.removeChild(element);
+	list.removeChild(element);
 
-  if (list.children.length === 0) {
-    container.classList.remove('show-container');
-  }
-  displayAlert('item removed', 'danger');
+	if (list.children.length === 0) {
+		container.classList.remove("show-container");
+	}
+	displayAlert("item removed", "danger");
 
-  setBackToDefault();
-  // remove from local storage
-  removeFromLocalStorage(id);
+	setBackToDefault();
+	removeFromLocalStorage(id);
 };
-// edit item
+
+
+// how to add price, qty, and total to editItem???
+
 const editItem = (e) => {
-  const element = e.currentTarget.parentElement.parentElement;
-  // set edit item
-  editElement = e.currentTarget.parentElement.previousElementSibling;
-  // set form value
-  newItem.value = editElement.innerHTML;
-  editFlag = true;
-  editID = element.dataset.id;
-  //
-  submitBtn.textContent = 'edit';
+	const element = e.currentTarget.parentElement.parentElement;
+	editElement = e.currentTarget.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling;
+	editPrice = e.currentTarget.parentElement.previousElementSibling.previousElementSibling.previousElementSibling;
+	editQty = e.currentTarget.parentElement.previousElementSibling.previousElementSibling;
+	editTotal = e.currentTarget.parentElement.previousElementSibling;
+	listInput.value = editElement.innerHTML;
+	priceInput.value = editPrice.innerHTML;
+	qtyInput.value = editQty.innerHTML;
+	editTotal.innerHTML = priceInput.value * qtyInput.value;
+	editFlag = true;
+	editID = element.dataset.id;
+	submitBtn.textContent = "edit";
 };
-// set backt to defaults
 const setBackToDefault = () => {
-  newItem.value = '';
-  editFlag = false;
-  editID = '';
-  submitBtn.textContent = 'submit';
+	listInput.value = "";
+    priceInput.value = "";
+    qtyInput.value = "";
+	editFlag = false;
+	editID = "";
+	submitBtn.textContent = "submit";
 };
 
-// ****** local storage **********
-
-// add to local storage
-const addToLocalStorage = (id, title) => {
-  const newItem = { id, title };
-  let items = getLocalStorage();
-  items.push(newItem);
-  localStorage.setItem('list', JSON.stringify(items));
+const addToLocalStorage = (id, value, price, qty, total) => {
+	const listInput = { id, value, price, qty, total };
+	let items = getLocalStorage();
+	items.push(listInput);
+	localStorage.setItem("list", JSON.stringify(items));
 };
 
 const getLocalStorage = () => {
-  return localStorage.getItem('list')
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
+	return localStorage.getItem("list")
+		? JSON.parse(localStorage.getItem("list"))
+		: [];
 };
 
 const removeFromLocalStorage = (id) => {
-  let items = getLocalStorage();
+	let items = getLocalStorage();
 
-  items = items.filter(function (item) {
-    if (item.id !== id) {
-      return item;
-    }
-  });
+	items = items.filter(function (item) {
+		if (item.id !== id) {
+			return item;
+		}
+	});
 
-  localStorage.setItem('list', JSON.stringify(items));
+	localStorage.setItem("list", JSON.stringify(items));
 };
 
-const editLocalStorage = (id, title) => {
-  let items = getLocalStorage();
+const editLocalStorage = (id, value, price, qty, total) => {
+	let items = getLocalStorage();
 
-  items = items.map(function (item) {
-    if (item.id === id) {
-      item.value = title;
-    }
-    return item;
-  });
-  localStorage.setItem('list', JSON.stringify(items));
+	items = items.map(function (item) {
+		if (item.id === id) {
+			item.value = value;
+			item.price = price;
+			item.qty = qty;
+			item.total = total;
+		}
+		return item;
+	});
+	localStorage.setItem("list", JSON.stringify(items));
 };
-
-// SETUP LOCALSTORAGE.REMOVEITEM('LIST');
-
-// ****** setup items **********
 
 const setupItems = () => {
-  let items = getLocalStorage();
+	let items = getLocalStorage();
 
-  if (items.length > 0) {
-    items.forEach(function (item) {
-      createListItem(item.id, item.title);
-    });
-    container.classList.add('show-container');
-  }
+	if (items.length > 0) {
+		items.forEach(function (item) {
+			createListItem(item.id, item.value, item.price, item.qty, item.total);
+		});
+		container.classList.add("show-container");
+	}
 };
 
-const createListItem = (id, title) => {
-  const element = document.createElement('div');
-  let pKey = document.createAttribute('data-id');
-  pKey.value = id;
-  element.setAttributeNode(pKey);
-  element.classList.add('item');
-  element.innerHTML = `<p class="title">${title}</p>
+const createListItem = (id, value, price, qty, total) => {
+	const element = document.createElement("article");
+	let attr = document.createAttribute("data-id");
+	attr.value = id;
+	element.setAttributeNode(attr);
+	element.classList.add("list-item");
+	element.innerHTML = `<p class="title">${value}</p>
+            <p class="price">${price}</p>
+            <p class="qty">${qty}</p>
+            <p class="total">${total}</p>
             <div class="btn-container">
-              <!-- edit btn -->
-              <button type="button" class="edit-btn">
+            <button type="button" class="edit-btn">
                 <i class="fas fa-edit"></i>
-              </button>
-              <!-- delete btn -->
-              <button type="button" class="delete-btn">
+            </button>
+            <button type="button" class="delete-btn">
                 <i class="fas fa-trash"></i>
-              </button>
+            </button>
             </div>
-          `;
-  // add event listeners to both buttons;
-  const deleteBtn = element.querySelector('.delete-btn');
-  deleteBtn.addEventListener('click', deleteItem);
-  const editBtn = element.querySelector('.edit-btn');
-  editBtn.addEventListener('click', editItem);
+        `;
+	const deleteBtn = element.querySelector(".delete-btn");
+	deleteBtn.addEventListener("click", deleteItem);
+	const editBtn = element.querySelector(".edit-btn");
+	editBtn.addEventListener("click", editItem);
 
-  // append child
-  list.appendChild(element);
+	list.appendChild(element);
 };
 
-// ****** event listeners **********
 
-// submit form
-form.addEventListener('submit', addItem);
-// clear list
-clearBtn.addEventListener('click', clearItems);
-// display items onload
-window.addEventListener('DOMContentLoaded', setupItems);
+
+form.addEventListener("submit", addItem);
+clearBtn.addEventListener("click", clearItems);
+window.addEventListener("DOMContentLoaded", setupItems);
